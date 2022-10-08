ad2badf793737b0b5686eb4dac740eb136343b32
anwjrrp33 <anwjrrp33@gmail.com>
2022-10-09 00:28:36 +0900

    refactor: 시도 횟수 검증 널, 공백, 제로 기능 추가

--- src/test/java/racingcar/model/GameRoundTest.java
+++ src/test/java/racingcar/model/GameRoundTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.code.GameRoundErrorCode;
 
@@ -16,5 +17,32 @@ public class GameRoundTest {
             () -> { new GameRound(round); });
 
         assertEquals(GameRoundErrorCode.NUMBER.getMessage(), exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 시도_횟수_NULL_확인(String round) {
+        RuntimeException exception = assertThrows(IllegalArgumentException.class,
+            () -> { new GameRound(round); });
+
+        assertEquals(GameRoundErrorCode.NULL.getMessage(), exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { " ", "   " })
+    void 시도_횟수_공백_확인(String round) {
+        RuntimeException exception = assertThrows(IllegalArgumentException.class,
+            () -> { new GameRound(round); });
+
+        assertEquals(GameRoundErrorCode.EMPTY.getMessage(), exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "0" })
+    void 시도_횟수_0회_확인(String round) {
+        RuntimeException exception = assertThrows(IllegalArgumentException.class,
+            () -> { new GameRound(round); });
+
+        assertEquals(GameRoundErrorCode.ZERO.getMessage(), exception.getMessage());
     }
 }
